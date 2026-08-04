1b745ed9a191e58b928804331958b7548ff2ef8c
sobhan <sobhanjafarii87@gmail.com>
2026-08-04 13:46:31 +0330

    [ADD] article button component

---
+++ src/features/article/Button.tsx
@@ -0,0 +1,13 @@
+const ButtonArticle = () => {
+  return (
+    <section className="w-fit flex justify-between items-center gap-2 py-3 px-5 rounded-full border border-[#0d5559] bg-[#0a3a42] text-[#2de4cc]">
+      <div className="w-2.5 aspect-square rounded-full bg-[#2dd4bf]">
+        <div className="w-2.5 aspect-square rounded-full bg-[#2dd4bf] animate-ping"></div>
+      </div>
+
+      <span className="text-sm">همراه نوآوری و فناوری</span>
+    </section>
+  );
+};
+
+export default ButtonArticle;
