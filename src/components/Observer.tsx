bf7f6b2c2e82e5d958ebf893119574ebc579a91f
sobhan <sobhanjafarii87@gmail.com>
2026-08-04 17:29:59 +0330

    [FIX] delay observer

--- src/components/Observer.tsx
+++ src/components/Observer.tsx
@@ -1,20 +1,28 @@
 import { ComponentProps } from "react";
 
 type Props = ComponentProps<"section"> & {
-  delay: number;
+  delay?: "100" | "150" | "200" | "300" | "500" | "700" | "1000" | "initial";
   observer: boolean;
 };
 
 const Observer = ({
   className,
   children,
-  delay,
+  delay = "500",
   observer,
   ...otherProps
 }: Props) => {
   return (
     <section
-      className={`w-full transition-all duration-${delay.toString()} ease-in-out ${observer ? "translate-y-0" : "translate-y-10 opacity-0"} ${className}`}
+      className={`w-full transition-all ease-in-out ${observer ? "translate-y-0" : "translate-y-10 opacity-0"} ${className}
+        ${delay === "100" && "duration-100"}
+        ${delay === "150" && "duration-150"}
+        ${delay === "200" && "duration-200"}
+        ${delay === "300" && "duration-300"}
+        ${delay === "500" && "duration-500"}
+        ${delay === "700" && "duration-700"}
+        ${delay === "1000" && "duration-1000"}
+        ${delay === "initial" && "duration-initial"}`}
       {...otherProps}
     >
       {children}
