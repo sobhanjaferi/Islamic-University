3d8631f21cc29c19d5cd72357f8831241dc659ac
sobhan <sobhanjafarii87@gmail.com>
2026-08-04 16:27:16 +0330

    [REFACTORE] button component

--- src/features/article/Button.tsx
+++ src/features/article/Button.tsx
@@ -1,6 +1,13 @@
-const ButtonArticle = () => {
+import { ComponentProps } from "react";
+
+type Props = ComponentProps<"section">;
+
+const ButtonArticle = ({ className, ...otherProps }: Props) => {
   return (
-    <section className="w-fit flex justify-between items-center gap-2 py-3 px-5 rounded-full border border-[#0d5559] bg-[#0a3a42] text-[#2de4cc]">
+    <section
+      className={`w-fit flex justify-between items-center gap-2 py-3 px-5 rounded-full border border-[#0d5559] bg-[#0a3a42] text-[#2de4cc] ${className}`}
+      {...otherProps}
+    >
       <div className="w-2.5 aspect-square rounded-full bg-[#2dd4bf]">
         <div className="w-2.5 aspect-square rounded-full bg-[#2dd4bf] animate-ping"></div>
       </div>
